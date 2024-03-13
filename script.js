7cc283919e46736b51aeeff12bce7ee008aa1e3d
Juan <siriuslivebr@gmail.com>
2024-03-12 22:37:03 -0300

    ajeitando bugs part2

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 function consultar(){
 
-  const cep = document.getElementById("cep").value
-  const resultado = document.getElementById("result")
-  
-  if(cep.length !== 8){
-    resultado.innerHTML = `CEP inválido, verifique os dados inseridos e tente novamente.`;
-    return;
+const cep = document.getElementById('cep').value
+const resultado = document.getElementById('result')
+
+  if (cep.length !== 8){
+  resultado.innerHTML = `Cep inválido`
+  return;
   }
-  
-  const url = `https://viacep.com.br/ws/${cep}/json/`
+
+const url = `https://viacep.com.br/ws/${cep}/json/`
 
   fetch(url)
-  .then(response =>{
-    return response.json();
-  })
-  .then(data =>{
-    resultado.innerHTML = 
-    `
-    CEP: ${data.cep}, Logradouro: ${data.logradouro}, Complemento: ${data.complemento}, Bairro: ${data.bairro}, Cidade: ${data.localidade},
-    UF: ${data.uf}, DDD: ${data.ddd}
-    `
-  })
-
+    .then(response =>{
+      return response.json();
+    })
+    .then(data =>{
+      resultado.innerHTML = `CEP: ${data.cep} | Logradouro: ${data.logradouro} | Complemento: ${data.complemento} | Bairro: ${data.bairro}
+       | Cidade: ${data.localidade} | UF: ${data.uf} | DDD: ${data.ddd}`
+    })
+    .catch(err =>{
+      console.error(`Erro ao consultar CEP`, err)
+    })
 }
